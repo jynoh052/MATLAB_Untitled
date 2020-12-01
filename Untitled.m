00ab404df27874ae5fd2fd579d5484c9bfbc36d4
jynoh052 <uk052@kaist.ac.kr>
2020-12-01 20:55:54 +0900

    Update Untitled.m

--- Untitled.m
+++ Untitled.m
@@ -2,3 +2,16 @@ dt=1/400;
 fc=20;
 rc=1/fc/2/pi;
 alpha=dt/(dt+rc)
+<<<<<<< HEAD
+=======
+
+%  test1
+%  test22
+<<<<<<< HEAD
+%  test33
+%  test44
+% aaaaa
+=======
+%  test33
+>>>>>>> parent of c92a87e... Update Untitled.m
+>>>>>>> parent of 4437ed0... Update Untitled.m
