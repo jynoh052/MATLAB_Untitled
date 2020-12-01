be1060257b39cb39c33f77e5f6e93202b220f31d
jynoh052 <uk052@kaist.ac.kr>
2020-12-01 21:00:21 +0900

    Update Untitled.m

--- Untitled.m
+++ Untitled.m
@@ -1,30 +1,4 @@
 dt=1/400;
 fc=20;
 rc=1/fc/2/pi;
-alpha=dt/(dt+rc)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 43d1522... Update Untitled.m
-
-%  test1
-%  test22
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> parent of 43d1522... Update Untitled.m
-%  test33
-%  test44
-% aaaaa
-=======
-%  test33
->>>>>>> parent of c92a87e... Update Untitled.m
-<<<<<<< HEAD
->>>>>>> parent of 4437ed0... Update Untitled.m
-=======
-=======
-%  test33
->>>>>>> parent of c92a87e... Update Untitled.m
->>>>>>> parent of 43d1522... Update Untitled.m
+alpha=dt/(dt+rc);
