43d1522abc93028b9c20e0f147edcf674c42d1ad
jynoh052 <uk052@kaist.ac.kr>
2020-12-01 20:54:36 +0900

    Update Untitled.m

--- Untitled.m
+++ Untitled.m
@@ -2,17 +2,3 @@ dt=1/400;
 fc=20;
 rc=1/fc/2/pi;
 alpha=dt/(dt+rc)
-
-%  test1
-%  test22
-<<<<<<< HEAD
-<<<<<<< HEAD
-%  test33
-%  test44
-% aaaaa
-=======
-%  test33
->>>>>>> parent of c92a87e... Update Untitled.m
-=======
-%  test33
->>>>>>> parent of c92a87e... Update Untitled.m
